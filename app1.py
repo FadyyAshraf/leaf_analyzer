005d380f53377aacb3f54bad6799255277d9aa79
FadyyAshraf <161021160+FadyyAshraf@users.noreply.github.com>
2025-05-29 15:05:39 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import torch
 from torchvision import transforms
 from tensorflow.keras.applications.mobilenet_v3 import preprocess_input
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
