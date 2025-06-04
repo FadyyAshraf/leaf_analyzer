bba0e543a63dadd6c70923fb58dd31416e26da37
FadyyAshraf <161021160+FadyyAshraf@users.noreply.github.com>
2025-06-04 05:21:32 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -153,5 +153,5 @@ def analyze():
         "message":         "Analysis complete"
     })
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
